e44fcf6bb937d657a09fe8c7f89e2f5d7860443e
lacobham <146376068+lacobham@users.noreply.github.com>
2024-01-25 21:56:50 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -72,7 +72,7 @@ async function initMap() {
   // A marker using a Font Awesome icon for the glyph.
   const icon = document.createElement("div");
 
-  icon.innerHTML = '<box-icon type='solid' name='tree-alt'></box-icon>';
+  icon.innerHTML = '<i class='bx bxs-tree-alt'></i>';
 
   const faPin = new PinElement({
     glyph: icon,
