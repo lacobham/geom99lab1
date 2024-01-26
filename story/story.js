b4f8f03872e746d4b4e83c62bff6bb2048c023f5
lacobham <146376068+lacobham@users.noreply.github.com>
2024-01-25 21:24:57 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -6,16 +6,45 @@ async function initMap() {
   );
   const { Place } = await google.maps.importLibrary("places");
   const map = new Map(document.getElementById("map"), {
-    center: {lat: 45.9701, lng: -81.5080},
-    zoom: 14,
+    center: {
+      lat: 45.9701,
+      lng: -81.5080
+    },
+    zoom: 15,
     mapId: "4504f8b37365c3d0",
   });
   const parser = new DOMParser();
+    // A marker with a with a URL pointing to a PNG.
+  const dinnerplateImg = document.createElement("img");
+  dinnerplateImg.src =
+    "dinner.jpg";
+  const dinnerplateMarkerView = new AdvancedMarkerElement({
+    map,
+    position: {
+      lat: 45.971284876286475,
+      lng: -81.51550012822338
+    },
+    content: dinnerplateImg,
+    title: "A marker using a custom PNG Image",
+  });
+  // A marker with a custom SVG glyph.
+  const glyphImg = document.createElement("img");
+  glyphImg.src = 'hotel.svg';
+  const glyphSvgPinElement = new PinElement({
+    glyph: glyphImg,
+  });
+  const glyphSvgMarkerView = new AdvancedMarkerElement({
+   position: {
+      lat: 45.970074027456945,
+      lng: -81.50804157729885
+    },
+    content: glyphSvgPinElement.element,
+    title: "A marker using a custom SVG for the glyph.",
+  });
   // A marker customized using a place icon and color, name, and geometry.
   const place = new Place({
     id: "ChIJBWXVQVZzLk0RDaSVKMHkanE",
   });
-
   // Call fetchFields, passing the desired data fields.
   await place.fetchFields({
     fields: [
@@ -25,7 +54,6 @@ async function initMap() {
       "iconBackgroundColor",
     ],
   });
-
   const pinElement = new PinElement({
     background: place.iconBackgroundColor,
     glyph: new URL(String(place.svgIconMaskURI)),
@@ -36,30 +64,25 @@ async function initMap() {
     content: pinElement.element,
     title: place.displayName,
   });
-  // A marker with a with a URL pointing to a PNG.
-  const dinnerplateImg = document.createElement("img");
-
-  dinnerplateImg.src =
-    "dinner.jpg";
-  const dinnerplateMarkerView = new AdvancedMarkerElement({
-    map,
-    position: {lat: 45.971284876286475, lng: -81.51550012822338},
-    content: dinnerplateImg,
-    title: "A marker using a custom PNG Image",
+  // A marker using a Font Awesome icon for the glyph.
+  const icon = document.createElement("div");
+  icon.innerHTML = '<i class="fa fa-mountain fa-lg"</i>';
+  const faPin = new PinElement({
+    glyph: icon,
+    glyphColor: "#c1d8f0",
+    background: "#4663ac",
+    borderColor: "##4a5778",
+    borderColor: "#4a5778",
   });
-  // A marker with a custom SVG glyph.
-  const glyphImg = document.createElement("img");
-
-  glyphImg.src = "hotel.svg";
-
-  const glyphSvgPinElement = new PinElement({
-    glyph: glyphImg,
-  });
-  const glyphSvgMarkerView = new AdvancedMarkerElement({
+  const faMarker = new AdvancedMarkerElement({
     map,
-    position: {lat: 45.970074027456945, lng: -81.50804157729885},
-    content: glyphSvgPinElement.element,
-    title: "A marker using a custom SVG for the glyph.",
+    position: {
+      lat: 45.96857337410628,
+      lng: -81.48900853545217
+    },
+    content: faPin.element,
+    title: "A marker using a FontAwesome icon for the glyph.",
   });
 }
+
 initMap();
