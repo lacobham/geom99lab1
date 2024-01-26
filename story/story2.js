c8482c52d3cac3031fc0a34e4ab33ace02cae42b
lacobham <146376068+lacobham@users.noreply.github.com>
2024-01-25 22:38:41 -0500

    Create story2.js

---
+++ story/story2.js
@@ -0,0 +1,87 @@
+async function initMap() {
+  // Request needed libraries.
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const center = { lat: 37.43238031167444, lng: -122.16795397128632 };
+  const map = new Map(document.getElementById("map"), {
+    zoom: 11,
+    center,
+    mapId: "4504f8b37365c3d0",
+  });
+
+  for (const property of properties) {
+    const AdvancedMarkerElement = new google.maps.marker.AdvancedMarkerElement({
+      map,
+      content: buildContent(property),
+      position: property.position,
+      title: property.description,
+    });
+
+    AdvancedMarkerElement.addListener("click", () => {
+      toggleHighlight(AdvancedMarkerElement, property);
+    });
+  }
+}
+
+function toggleHighlight(markerView, property) {
+  if (markerView.content.classList.contains("highlight")) {
+    markerView.content.classList.remove("highlight");
+    markerView.zIndex = null;
+  } else {
+    markerView.content.classList.add("highlight");
+    markerView.zIndex = 1;
+  }
+}
+
+function buildContent(property) {
+  const content = document.createElement("div");
+
+  content.classList.add("property");
+  content.innerHTML = `
+    <div class="icon">
+        <i aria-hidden="true" class="fa fa-icon fa-${property.type}" title="${property.type}"></i>
+        <span class="fa-sr-only">${property.type}</span>
+    </div>
+    <div class="details">
+        <div class="price">${property.price}</div>
+        <div class="address">${property.address}</div>
+        <div class="features">
+        <div>
+            <i aria-hidden="true" class="fa fa-bed fa-lg bed" title="bedroom"></i>
+            <span class="fa-sr-only">bedroom</span>
+            <span>${property.bed}</span>
+        </div>
+        <div>
+            <i aria-hidden="true" class="fa fa-bath fa-lg bath" title="bathroom"></i>
+            <span class="fa-sr-only">bathroom</span>
+            <span>${property.bath}</span>
+        </div>
+        <div>
+            <i aria-hidden="true" class="fa fa-ruler fa-lg size" title="size"></i>
+            <span class="fa-sr-only">size</span>
+            <span>${property.size} ft<sup>2</sup></span>
+        </div>
+        </div>
+    </div>
+    `;
+  return content;
+}
+
+const properties = [
+  {
+    address: "215 Emily St, MountainView, CA",
+    description: "Single family house with modern design",
+    price: "$ 3,889,000",
+    type: "home",
+    bed: 5,
+    bath: 4.5,
+    size: 300,
+    position: {
+      lat: 37.50024109655184,
+      lng: -122.28528451834352,
+    },
+  },
+  {
+];
+
+initMap();
